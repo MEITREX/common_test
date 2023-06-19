b7b6fa7a3825b2d5801003c56d42a8f764a1d171
Rick Zolnierek <sjatah@gmail.com>
2023-06-19 12:00:47 +0200

    removed unneeded annotation

--- src/main/java/de/unistuttgart/iste/gits/common/testutil/GraphQlApiTest.java
+++ src/main/java/de/unistuttgart/iste/gits/common/testutil/GraphQlApiTest.java
@@ -17,7 +17,6 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@ActiveProfiles("test")
 @Testcontainers
 public @interface GraphQlApiTest {
 }
