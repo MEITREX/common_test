bccdf27011be758a50bdf51cf91758f5af1017b5
Rick Zolnierek <sjatah@gmail.com>
2023-06-19 11:47:03 +0200

    added annotations to GraphQlApiTest

--- src/main/java/de/unistuttgart/iste/gits/common/testutil/GraphQlApiTest.java
+++ src/main/java/de/unistuttgart/iste/gits/common/testutil/GraphQlApiTest.java
@@ -2,6 +2,8 @@ package de.unistuttgart.iste.gits.common.testutil;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.lang.annotation.*;
 
@@ -15,5 +17,7 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
+@ActiveProfiles("test")
+@Testcontainers
 public @interface GraphQlApiTest {
 }
