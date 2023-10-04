d7244c5528346114b00e6c2cd5eed6b911adcfcd
Paul Bredl <34026908+PaulBredl@users.noreply.github.com>
2023-10-04 13:53:58 +0200

    Fix json current user header (#10)
    * fix json creation
    
    * add example users for tests
    
    * add AuthorizationAsserts

---
+++ src/main/java/de/unistuttgart/iste/gits/common/testutil/AuthorizationAsserts.java
@@ -0,0 +1,28 @@
+package de.unistuttgart.iste.gits.common.testutil;
+
+import lombok.NoArgsConstructor;
+import org.springframework.graphql.ResponseError;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * This class contains asserts for authorization.
+ */
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class AuthorizationAsserts {
+
+    /**
+     * Asserts that the given graphqlErrors contain only a missing user role error.
+     *
+     * @param graphqlErrors the graphql errors
+     */
+    public static void assertIsMissingUserRoleError(final List<ResponseError> graphqlErrors) {
+        assertThat(graphqlErrors, hasSize(1));
+        assertThat(graphqlErrors.get(0).getExtensions().get("exception"), is("NoAccessToCourseException"));
+        assertThat(graphqlErrors.get(0).getMessage(), containsString("User does not have the required role to access this data of the course."));
+
+    }
+}
